49da7e47b9c94757c152fb9a4c2b4d020f5dec54
newjusty <newsweettime@gmail.com>
2025-02-08 22:05:17 +0700

    เพิ่ม Navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // ไฟล์สำหรับสไตล์
+
+const Navbar = () => {
+  return (
+    <div className="Navbarspace">
+        <header className="navbar">
+            <img src="/PM1.1.png" alt="Logo" className="navbar-logo" />
+            <nav-center>
+                <Link to="/listtogo">List to Go</Link>
+                <Link to="/planner">Planner</Link>
+                <Link to="/aboutus">About us</Link>
+            </nav-center>
+            <Link to="/profile" className="nav-profile">Profile</Link>
+        </header>
+    </div>
+  );
+};
+
+export default Navbar;
